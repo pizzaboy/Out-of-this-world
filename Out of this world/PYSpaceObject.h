119c7915ac355d492197ab8a30fa8f696ad2f93e
Pete Yates <pyates@me.com>
2014-03-21 16:56:13 +0000

    New files for PYSpaceObject Class

---
+++ Out of this world/PYSpaceObject.h
@@ -0,0 +1,13 @@
+//
+//  PYSpaceObject.h
+//  Out of this world
+//
+//  Created by Pete Yates on 21/03/2014.
+//  Copyright (c) 2014 Pete Yates. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PYSpaceObject : NSObject
+
+@end
---
+++ Out of this world/PYSpaceObject.m
@@ -0,0 +1,13 @@
+//
+//  PYSpaceObject.m
+//  Out of this world
+//
+//  Created by Pete Yates on 21/03/2014.
+//  Copyright (c) 2014 Pete Yates. All rights reserved.
+//
+
+#import "PYSpaceObject.h"
+
+@implementation PYSpaceObject
+
+@end
